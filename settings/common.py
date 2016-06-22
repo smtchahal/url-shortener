300f2966622d2b5cbce772862eff978604aa490d
Sumit Chahal <smtchahal@gmail.com>
2016-06-22 17:16:01 +0530

    Move to CDN for static files

--- settings/common.py
+++ settings/common.py
@@ -31,7 +31,6 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
 ]
 
 MIDDLEWARE_CLASSES = [
@@ -101,14 +100,3 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.9/howto/static-files/
-
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATIC_URL = '/static/'
-
-STATICFILES_DIR = (
-    os.path.join(BASE_DIR, 'static'),
-)
--- settings/heroku.py
+++ settings/heroku.py
@@ -2,13 +2,6 @@ import dj_database_url
 
 from .common import *
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATIC_URL = '/static/'
-
-STATICFILES_DIR = (
-    os.path.join(BASE_DIR, 'static'),
-)
-
 DATABASES = {
     'default': dj_database_url.config(),
 }
