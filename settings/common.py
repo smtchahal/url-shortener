1c5a3b6b5d2e0cf2ea672ff9d4bcfd6e71c71b67
Sumit Chahal <smtchahal@gmail.com>
2016-06-22 16:46:46 +0530

    Uhhh

--- settings/common.py
+++ settings/common.py
@@ -105,3 +105,10 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_URL = '/static/'
+
+STATICFILES_DIR = (
+    os.path.join(BASE_DIR, 'static'),
+)
