e32a03cac9ff93ba2574487650c7c53b675ebe61
Sumit Chahal <smtchahal@gmail.com>
2016-07-01 20:39:35 +0530

    Added static files support in Heroku

--- url_shortener/urls.py
+++ url_shortener/urls.py
@@ -1,3 +1,6 @@
+import os
+
+from django.conf import settings
 from django.conf.urls import url
 
 from . import views
@@ -10,3 +13,9 @@ urlpatterns = [
     url(r'^(?P<alias>[a-zA-Z0-9-_]+)\+$', views.preview, name='preview'),
     url(r'^~analytics/$', views.analytics, name='analytics'),
 ]
+
+# For Heroku
+if 'DYNO' in os.environ:
+    urlpatterns += [
+        url(r'^~static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
+    ]
